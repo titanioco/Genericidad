11ec6284ce685c5df14c25a6a3fca9b6d6c8b4a5
Alejandro Acosta <diacostamo@unal.edu.co>
2024-08-29 11:06:42 -0500

    Update Pair.java

--- src/main/java/objetos/genericidad/Pair.java
+++ src/main/java/objetos/genericidad/Pair.java
@@ -17,6 +17,30 @@ public class Pair {
         System.out.println("Datos Persona" + datosPersona);
         
         Dato<String, String> 
+        
+        public class Pair<T, U> {
+            
+            private T first;
+            private U second;
+            
+            public Pair(T first, U second) {
+                this.first = first;
+                this.second = second;
+            }
+            
+            public T getFirst() {
+                return first;
+            }
+            
+            public U getSecond() {
+                return second;
+            }
+            
+            @Override
+            public String toString() {
+                return "(" + first + ", " + second + ")";
+            }
+        }
           
     }
 }
