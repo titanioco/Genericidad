0fc46a7bb1c5b1a8f2ffa6ea185a7bd11e4793cc
ESTUDIANTES <ESTUDIANTES@10.220.57.19>
2024-08-29 10:57:31 -0500

    first commit

---
+++ src/main/java/objetos/genericidad/Pair.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package objetos.genericidad;
+
+/**
+ *
+ * @author ESTUDIANTES
+ */
+public class Pair {
+
+    public static void main(String[] args) {
+        System.out.println("---Hola, En este programa podrás registrar datos de edad y nombre"
+                + "post--- ");
+        Pair<Integer,String> datosPersona = new Dato<>(25, "Leonardo");
+        System.out.println("Datos Persona" + datosPersona);
+        
+        Dato<String, String> 
+          
+    }
+}
